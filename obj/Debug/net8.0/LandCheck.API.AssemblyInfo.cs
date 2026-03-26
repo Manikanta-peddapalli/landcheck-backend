8a51c9988704c53e4ed07650ca7a9c63d8aed2dc
unknown <manikanta1742@gmail.com>
2026-03-26 13:22:01 +0530

    fix: remove Tesseract and QuestPDF crash

--- obj/Debug/net8.0/LandCheck.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/LandCheck.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LandCheck.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ca162ac1f092bde7b2a930d0751d4676cfe937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4629befc7110acaca417511851b141d14c6acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LandCheck.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LandCheck.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
